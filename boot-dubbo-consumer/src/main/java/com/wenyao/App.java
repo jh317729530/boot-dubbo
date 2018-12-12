ee62c610ca28db05313a0eb79ef5481bbb4d8251
ganjunhui <junhui_g@gzleihou.cn>
2018-12-12 23:10:57 +0800

    初始化项目

---
+++ boot-dubbo-consumer/src/main/java/com/wenyao/App.java
@@ -0,0 +1,13 @@
+package com.wenyao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

0f75c24c23c47be39cff7b83f33fb3e99d742807
ganjunhui <junhui_g@gzleihou.cn>
2018-12-13 20:53:20 +0800

    先不自动配置datasource

--- boot-dubbo-provider/src/main/java/com/wenyao/ProviderApplication.java
+++ boot-dubbo-provider/src/main/java/com/wenyao/ProviderApplication.java
@@ -2,9 +2,10 @@ package com.wenyao;
 
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ProviderApplication {
 
     public static void main(String[] args) {

d1de15dcbe58369f9c1b9292c0d991596b83ede0
ganjunhui <317729530@qq.com>
2018-12-15 00:44:45 +0800

    修改包扫描位置

--- boot-dubbo-consumer/src/main/java/com/wenyao/ConsumerApplication.java
+++ boot-dubbo-consumer/src/main/java/com/wenyao/ConsumerApplication.java
@@ -1,10 +1,12 @@
 package com.wenyao;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "com.wenyao.controller",exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = "com.wenyao",exclude = {DataSourceAutoConfiguration.class})
 public class ConsumerApplication {
 
     public static void main(String[] args) {

cadef444ec7b413640478be059525f6820e4205b
ganjunhui <317729530@qq.com>
2018-12-15 00:47:48 +0800

    分页常量

---
+++ boot-dubbo-common/src/main/java/com/wenyao/constant/PageConst.java
@@ -0,0 +1,28 @@
+package com.wenyao.constant;
+
+/**
+ * 分页常量
+ * @author ganjunhui
+ */
+public class PageConst {
+
+    /**
+     * 默认页码
+     */
+    public static final int PAGE_NO = 1;
+
+    /**
+     * 默认每页显示条数
+     */
+    public static final int PAGE_SIZE = 10;
+
+    /**
+     * 分页请求参数名常量
+     */
+    public static class PageParameter {
+
+        public static final String PAGE_NO = "pageNo";
+
+        public static final String PAGE_SIZE = "pageSize";
+    }
+}
